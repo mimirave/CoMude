8b2f44faa218539a74e99a86f94aaf982b797e9f
mimirave <33813706+mimirave@users.noreply.github.com>
2020-08-27 17:23:40 -0400

    Delete game.js

--- game.js
+++
@@ -1,9 +0,0 @@
-
-    // This will run when a bookmark is created.
-chrome.bookmarks.onCreated.addListener(function (id, bookmarkInfo) {
-  
-        window.open("https://www.a2gov.org/departments/police/Pages/default.aspx", "_blank")
-        console.log("Dangerous triggering event fired: police help requested")
-
-});
-
