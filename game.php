420b303a709df5ae0b271a0e7a76ee54efd14ae2
mimirave <33813706+mimirave@users.noreply.github.com>
2020-08-27 20:52:28 -0400

    Update game.php

--- game.php
+++ game.php
@@ -4,3 +4,9 @@ $QandAarray = array(
 	array("three", 0, 0, 0, 0, 0)
 );
 
+<script type="text/javascript">
+    var my_var = <?php json_encode($my_var); ?>;
+    if(my_var)
+	    <?php array_push($QandAarray["one"],$newdata); ?>
+</script>
+
