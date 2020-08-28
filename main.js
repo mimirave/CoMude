c15eeeaaae01b73751e51b41f329041add7e916a
mimirave <33813706+mimirave@users.noreply.github.com>
2020-08-27 20:01:56 -0400

    Removed chrome extension-specific javascript

--- main.js
+++ main.js
@@ -1,20 +1,2 @@
-// This event is fired when the user hits "enter"
-//chrome.omnibox.onInputEntered.addListener(omniChanged);
-function omniChanged(text) {
-    // The variable "text" has the text the user typed
-    // You could open a new tab on a specific page that uses that text
-    // or send a message to a content script
-    // etc.
-}
 
-// This will run when a bookmark is created.
-/*chrome.bookmarks.onCreated.addListener(function (id, bookmarkinfo) {
-    console.log(bookmarkinfo.title);
-    chrome.bookmarks.get(id, function () {
-        if (id.title == "help") {
-            window.open("https://www.a2gov.org/departments/police/Pages/default.aspx", "_blank")
-            console.log("Dangerous triggering event fired: police help requested")
-        }
-    })
-});*/
 
