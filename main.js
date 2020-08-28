7b7893bd6c358c47812f082682dc757d8bfbcc4f
mimirave <33813706+mimirave@users.noreply.github.com>
2020-08-27 23:04:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 let levelOnePrompt = ["In general, do you feel sad?", "Did you eat breakfast today?", "Are you safe?"];
+let levelTwoPrompt = ["Do you hear a lot of loud fighting in your house?", "Do you prefer to be in your own bedroom when at home?", "Do you feel valued at home?"]
+let levelThreePrompt = ["Are you positive that you will eat dinner tonight and tomorrow?", "Has anyone in your house hurt you recently?", "Has someone in your house made you feel unworthy of love?"]
 sessionStorage.setItem("Mild", JSON.stringify(levelOnePrompt));
+sessionStorage.setItem("Medium", JSON.stringify(levelTwoPrompt));
+sessionStorage.setItem("Intense", JSON.stringify(levelTheePrompt));
 
 function myFunction(data) {
   // Get the existing data
