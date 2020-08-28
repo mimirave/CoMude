b923fee64fbcfe8de551c474a7a0a486288d058b
mimirave <33813706+mimirave@users.noreply.github.com>
2020-08-27 21:35:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-function myFunction() {
-  
+function myFunction(data) {
+  // Get the existing data
+var existing = localStorage.getItem('responses');
+
+// If no existing data, create an array
+// Otherwise, convert the localStorage string to an array
+existing = existing ? existing.split(',') : [];
+
+// Add new data to localStorage Array
+existing.push(data.toString());
+
+// Save back to localStorage
+localStorage.setItem('responses', existing.toString());
 }
 
